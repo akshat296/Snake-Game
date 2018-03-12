4b7e03e2e0a4631e62c8900c7b6259efe6181987
Akshat <tandon.akshat@thinksys.com>
2018-03-12 12:11:25 +0530

    debug added

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
 //import { plugin } from "../../.cache/typescript/2.6/node_modules/postcss";
-
+const path = require('path');
+console.log(path.resolve(__dirname));
 module.exports = {
     entry: __dirname + "/src",
     output: {
         path: __dirname + "/js",
         filename: "bundle.js"
     },
-    devtool:'source-maps',
-    module:{
-        loaders:[
+    devtool: 'source-maps',
+
+    module: {
+        loaders: [
             {
                 test: /\.js$/,
-                loaders:'babel-loader',
-                query:{
-                    presets:['es2015','react'],
-                    plugins:['transform-class-properties']
+                loaders: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: ['transform-class-properties']
                 }
             },
             {
                 test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-              },
+                use: ['style-loader', 'css-loader']
+            },
 
-              {
+            {
                 test: /\.(png|jpg|gif|svg)$/,
                 use: [
-                  {
-                    loader: 'file-loader',
-                    options: {}  
-                  }
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
                 ]
-              }
-              
+            }
+
 
 
         ]
+    },
+    resolve: {
+        modules: [path.resolve(__dirname), "node_modules"]
     }
-    
-    
 }
